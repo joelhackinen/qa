c2d513f87d9f23d35a7e10c43b147d3b4975eb46
joel_laptop <joel.hakkinen@gmail.com>
2024-09-01 10:55:57 +0300

    rename css variable, making label and maxLength mandatory

--- qa-ui/src/components/TextArea.svelte
+++ qa-ui/src/components/TextArea.svelte
@@ -1,9 +1,7 @@
 <script>
-  const MAXLENGTH_DEFAULT = -1;
-
   export let id;
-  export let label = "";
-  export let maxLength = MAXLENGTH_DEFAULT;
+  export let label;
+  export let maxLength;
   export let body;
   export let focusHeight = 120;
 
@@ -21,12 +19,12 @@
 
 <div class="relative w-full px-2 py-4 truncate {$$restProps.class}">
   <textarea
-    style="--max-focus-height: {focusHeight}px"
+    style="--focus-height: {focusHeight}px"
     class="
       appearance-none w-full peer resize-none text-base outline-none hover:cursor-pointer focus:cursor-text
       ring-1 ring-offset-4 ring-gray-300 focus:ring-blue-400 rounded-md
       transition-all duration-200 focus:delay-100
-      h-8 focus:h-[var(--max-focus-height)] [&:not(:placeholder-shown)]:h-[var(--max-focus-height)] max-h-60
+      h-8 focus:h-[var(--focus-height)] [&:not(:placeholder-shown)]:h-[var(--focus-height)] max-h-60
     "
     id={id}
     name={id}
@@ -36,31 +34,27 @@
     placeholder=""
     spellcheck="false"
   />
-  {#if label}
-    <label
-      for={id}
-      class="
-        absolute z-10 bg-white hover:cursor-pointer peer-focus:cursor-text left-4 px-0.5 truncate
-        top-0.5 text-xs
-        peer-focus:top-0.5 peer-focus:text-xs peer-focus:text-black
-        peer-placeholder-shown:top-5 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500
-        transition-all duration-100
-        peer-[&:not(:focus)]:delay-200
-      "
-    >
-      {label}
-    </label>
-  {/if}
-  {#if maxLength !== MAXLENGTH_DEFAULT}
-    <label
-      for={id}
-      class="
-        absolute bg-white font-semibold hover:cursor-text px-0.5 text-xs truncate
-        bottom-2 left-4
-        peer-[&:not(:focus)]:peer-placeholder-shown:hidden
-      "
-    >
-      {remaining}
-    </label>
-  {/if}
+  <label
+    for={id}
+    class="
+      absolute z-10 bg-white hover:cursor-pointer peer-focus:cursor-text left-4 px-0.5 truncate
+      top-0.5 text-xs
+      peer-focus:top-0.5 peer-focus:text-xs peer-focus:text-black
+      peer-placeholder-shown:top-5 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500
+      transition-all duration-100
+      peer-[&:not(:focus)]:delay-200
+    "
+  >
+    {label}
+  </label>
+  <label
+    for={id}
+    class="
+      absolute bg-white font-semibold hover:cursor-text px-0.5 text-xs truncate
+      bottom-2 left-4
+      peer-[&:not(:focus)]:peer-placeholder-shown:hidden
+    "
+  >
+    {remaining}
+  </label>
 </div>
