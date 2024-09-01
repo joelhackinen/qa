ce038c1285f405ab413e1e70721270ab85c39bd3
joel_laptop <joel.hakkinen@gmail.com>
2024-09-01 11:16:07 +0300

    socket store to support subscriptions from multiple components simultaneously

--- qa-ui/src/stores/socket.js
+++ qa-ui/src/stores/socket.js
@@ -1,50 +1,54 @@
 import { writable } from "svelte/store";
 
 export const createWebSocketStore = (url) => {
-  const { subscribe, set } = writable(null);
-
   /** @type {WebSocket} */
   let socket;
 
-  const connect = () => {
-    socket = new WebSocket(url);
+  let subscribers = 0;
 
-    socket.addEventListener("open", () => {
-      console.log("socket connected");
-      set(socket);
-    });
+  const { subscribe, set } = writable(null, () => {
+    if (!socket) {
+      socket = new WebSocket(url);
 
-    socket.addEventListener("close", () => {
-      console.log("socket closed");
-      set(null);
-    });
+      socket.addEventListener("open", () => {
+        console.log("socket connected");
+        set(socket);
+      });
 
-    socket.addEventListener("message", (e) => {
-      console.log(e);
-    });
+      socket.addEventListener("close", () => {
+        console.log("socket closed");
+        set(null);
+        socket = null;
+      });
+  
+      socket.addEventListener("message", (e) => {
+        console.log(e);
+      });
+  
+      socket.addEventListener("error", (e) => {
+        console.error(e);
+      });
+    }
+    subscribers += 1;
 
-    socket.addEventListener("error", (e) => {
-      console.error(e);
-    });
-  };
-
-  const disconnect = () => {
-    if (socket) socket.close();
-  };
+    return () => {
+      subscribers -= 1;
+      if (subscribers === 0 && socket) {
+        socket.close();
+      }
+    };
+  });
 
   const send = (m) => {
     if (socket && socket.readyState === WebSocket.OPEN) {
       socket.send(JSON.stringify(m));
       return;
     }
-    console.log("Socket not ready")
-  }
+    console.log("Socket not ready");
+  };
 
   return {
     subscribe,
-    connect,
-    disconnect,
     send,
   };
 };
-
