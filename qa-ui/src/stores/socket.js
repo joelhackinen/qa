de2521267db06b59cefa6987fa8fc54816f52ed0
Joel Häkkinen <joel.hakkinen@gmail.com>
2024-09-03 13:57:16 +0300

    closurize socket

--- qa-ui/src/stores/socket.js
+++ qa-ui/src/stores/socket.js
@@ -1,54 +1,94 @@
-import { writable } from "svelte/store";
+const reopenTimeouts = [2000, 5000, 10000, 30000, 60000];
 
-export const createWebSocketStore = (url) => {
+export const Socket = (() => {
   /** @type {WebSocket} */
-  let socket;
+  let socket, openPromise, reopenTimeoutHandler, connectionUrl;
+  let reopenCount = 0;
+  let subscriptions = 0;
 
-  let subscribers = 0;
+  const reopenTimeout = () => {
+    const n = reopenCount;
+    reopenCount++;
+    return reopenTimeouts[
+      n >= reopenTimeouts.length - 1 ? reopenTimeouts.length - 1 : n
+    ];
+  }
 
-  const { subscribe, set } = writable(null, () => {
-    if (!socket) {
-      socket = new WebSocket(url);
-
-      socket.addEventListener("open", () => {
-        console.log("socket connected");
-        set(socket);
-      });
-
-      socket.addEventListener("close", () => {
-        console.log("socket closed");
-        set(null);
-        socket = null;
-      });
-  
-      socket.addEventListener("message", (e) => {
-        console.log(e);
-      });
-  
-      socket.addEventListener("error", (e) => {
-        console.error(e);
-      });
+  const open = (url) => {
+    if (reopenTimeoutHandler) {
+      clearTimeout(reopenTimeoutHandler);
+      reopenTimeoutHandler = undefined;
     }
-    subscribers += 1;
 
-    return () => {
-      subscribers -= 1;
-      if (subscribers === 0 && socket) {
-        socket.close();
-      }
-    };
-  });
+    if (openPromise) return openPromise;
+    if (socket) return;
 
-  const send = (m) => {
-    if (socket && socket.readyState === WebSocket.OPEN) {
-      socket.send(JSON.stringify(m));
-      return;
+    console.log("Opening WebSocket connection to:", url);
+    socket = new WebSocket(url);
+
+    socket.addEventListener("close", (_) => {
+      console.log("WebSocket closed")
+      reopen(url);
+    });
+
+    openPromise = new Promise((resolve, reject) => {
+      socket.addEventListener("error", (error) => {
+        reject(error);
+        openPromise = undefined;
+        console.error("Opening WebSocket failed");
+      });
+      socket.addEventListener("open", (_) => {
+        reopenCount = 0;
+        resolve();
+        openPromise = undefined;
+        connectionUrl = url;
+        console.log("WebSocket connected to:", url);
+      });
+    });
+    return openPromise;
+  };
+
+  const reopen = (connectionUrl) => {
+    console.log("Trying to reopen WebSocket to", connectionUrl);
+    close();
+    if (subscriptions > 0) {
+      reopenTimeoutHandler = setTimeout(() => open(connectionUrl), reopenTimeout());
     }
-    console.log("Socket not ready");
+  };
+
+  const close = () => {
+    if (reopenTimeoutHandler) {
+      clearTimeout(reopenTimeoutHandler);
+    }
+    console.log("Closing WebSocket");
+    if (socket) {
+      socket.close();
+      socket = undefined;
+      connectionUrl = undefined;
+    }
+    console.log("WebSocket closed");
   };
 
   return {
-    subscribe,
-    send,
+    send: (value) => {
+      const send = () => socket.send(JSON.stringify(value));
+      if (socket.readyState !== WebSocket.OPEN) open().then(send);
+      else send();
+    },
+    connect: (url) => {
+      subscriptions++;
+      open(url);
+    },
+    disconnect: () => {
+      subscriptions--;
+      if (subscriptions === 0) {
+        close();
+      }
+    },
+    addEventListener: (event, callback) => {
+      const addListener = () => socket.addEventListener(event, callback);
+      if (socket.readyState !== WebSocket.OPEN) open().then(addListener);
+      else addListener();
+    },
   };
-};
+})();
