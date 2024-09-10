67714af1ef506fda6268945cf68447f89a3d7340
Joel Häkkinen <joel.hakkinen@gmail.com>
2024-09-10 19:31:01 +0300

    better tests

--- e2e-playwright/tests/test-1.spec.ts
+++ e2e-playwright/tests/test-1.spec.ts
@@ -1,27 +1,24 @@
 import { test, expect } from '@playwright/test';
+import { v4 as uuidv4 } from 'uuid';
 
 test('Rate limit of asking questions', async ({ page }) => {
-  const addQuestion = async () => {
+  const addQuestion = async (message: string) => {
     await page.getByRole("textbox").focus();
     await page.locator('[name=question-input]').click();
-    await page.locator('#question-input').fill('Hi!');
+    await page.locator('#question-input').fill(message);
     await page.locator('#send-question-button').click();
   };
-  let dialogShown = false;
-
-  page.on('dialog', async (dialog) => {
-    expect(dialog.type()).toBe('alert');
-
-    await dialog.accept();
-    dialogShown = true;
-  });
 
   await page.goto('http://localhost:7800/');
+
   await page.locator('#CS-E4770-link').click();
   
-  await addQuestion();
-  await addQuestion();
-  
-  await expect(page.locator('body')).toContainText('Hi!');
-  expect(dialogShown).toBe(true);
+  const firstMessage = uuidv4();
+  await addQuestion(firstMessage);
+  await expect(page.locator('body')).toContainText(firstMessage);
+
+  const secondMessage = uuidv4();
+  await addQuestion(secondMessage);
+
+  await expect(page.locator('body')).not.toContainText(secondMessage);
 });
--- e2e-playwright/tests/test-4.spec.ts
+++ e2e-playwright/tests/test-4.spec.ts
@@ -4,9 +4,8 @@ test('Question upvoting works', async ({ page }) => {
   await page.goto('http://localhost:7800/');
   await page.locator('#CS-E4770-link').click();
   const votebox = page.locator('#question-list >> [name=votebox]').last();
-  const upvotebutton = votebox.getByRole('button').first();
+  const upvotebutton = votebox.locator('[name=upvote-button]');
   const preClickValue = await votebox.textContent();
   await upvotebutton.click();
-  const postClickValue = await votebox.textContent();
-  expect(Number(postClickValue)).toBe(Number(preClickValue) + 1);
+  await expect(votebox).toHaveText(String(Number(preClickValue) + 1));
 });
