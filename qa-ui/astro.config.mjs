4b9cbea8a6256a88a7b419c3c7270f08a15fdc65
Joel Häkkinen <joel.hakkinen@gmail.com>
2024-09-10 14:29:13 +0300

    optimized production build

--- qa-ui/astro.config.mjs
+++ qa-ui/astro.config.mjs
@@ -2,11 +2,30 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
-export default defineConfig({
-  integrations: [svelte(), tailwind()],
-  server: {
-    port: 3000,
-    host: true
-  },
-  output: "server",
-});
+const makeDevConfig = () => {
+  return defineConfig({
+    integrations: [svelte(), tailwind()],
+    server: {
+      port: 3000,
+      host: true
+    },
+    output: "server",
+  });
+};
+
+const makeProdConfig = async () => {
+  const node = (await import("@astrojs/node")).default;
+
+  return defineConfig({
+    integrations: [svelte(), tailwind()],
+    server: {
+      port: 3000,
+    },
+    output: "server",
+    adapter: node({
+      mode: "standalone",
+    }),
+  });
+};
+
+export default import.meta.env.DEV ? makeDevConfig() : await makeProdConfig();
